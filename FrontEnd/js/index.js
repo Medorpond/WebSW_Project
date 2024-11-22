d9873c0b1e9eda22dd59167311916101fb82a7c0
Medorpond <stcwh@naver.com>
2024-11-22 14:20:48 +0900

    feat/ 동적 nav메뉴

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -3,9 +3,12 @@ let content = null;
 document.addEventListener("DOMContentLoaded", onDOMLoad);
 
 
-// DOM 로드 함수
+// DOM 초기화 함수
 function onDOMLoad(){
     content = document.getElementById("content");
+
+    // 네비게이션 메뉴 동적 생성
+    initNavMenu();
 
     // 리스너 장착, url 부여
     const navs = document.querySelectorAll("a[data-section]")
@@ -18,6 +21,54 @@ function onDOMLoad(){
         });
     });
 }
+
+function initNavMenu(){
+    const mainMenuItems = [
+        { name: "Main Nav 01", dataSection: "subpage"},
+        { name: "Main Nav 02", dataSection: "subpage"},
+        { name: "Main Nav 03", dataSection: "subpage"},
+        { name: "Main Nav 04", dataSection: "subpage"}
+    ];
+
+    const subMenuItems = [
+        { name: "Sub Nav 01", dataSection: "subpage"},
+        { name: "Sub Nav 02", dataSection: "subpage"}
+    ];
+
+    const mainNavMenu = document.getElementById("mainMenu");
+    const subNavMenu = document.getElementById("subMenu");
+    const sidebar_Main = document.getElementById("sidebarMain");
+    const sidebar_Sub = document.getElementById("sidebarSub");
+
+    // 메뉴 항목 생성 함수
+    const createMenuItems = (parentElement, menuItems) => {
+        menuItems.forEach((item) => {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+
+            a.href = "#";
+            a.textContent = item.name;
+            a.dataset.section = item.dataSection;
+            li.appendChild(a);
+            parentElement.appendChild(li);
+        });
+    };
+
+    createMenuItems(mainNavMenu, mainMenuItems);
+    createMenuItems(subNavMenu, subMenuItems);
+    createMenuItems(sidebar_Main, mainMenuItems);
+    createMenuItems(sidebar_Sub, subMenuItems);
+}
+
+
+
+
+
+
+
+
+
+
 
 // Subpage 로드 함수
 function loadPage(pageName) {
