6d35322944e9c1fc0ccde851486380de2c89369a
Medorpond <134402610+Medorpond@users.noreply.github.com>
2024-11-23 21:01:26 +0900

    feat/사이드바 구현 및 코드 구조 수정
    YES

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -5,23 +5,33 @@ document.addEventListener("DOMContentLoaded", onDOMLoad);
 
 // DOM 초기화 함수
 function onDOMLoad(){
+    // content 영역 설정
     content = document.getElementById("content");
 
-    // 네비게이션 메뉴 동적 생성
+    // 네비게이션 메뉴 동적 생성 및 초기화
     initNavMenu();
 
-    // 리스너 장착, url 부여
-    const navs = document.querySelectorAll("a[data-section]")
-
-    navs.forEach(nav => {
-        nav.addEventListener("click", (event) => {
-            event.preventDefault(); // 링크의 기본 동작(페이지 이동) 막기
-            const url = nav.getAttribute("data-section"); // URL 가져오기
-            loadPage(url); // 페이지 로드 함수 호출
-        });
-    });
+    //버튼 이벤트 초기화
+    initBtn();
 }
 
+// 버튼 동작 이벤트 추가 함수
+function initBtn(){
+    document.getElementById('openSidebarBtn').addEventListener('click', openSidebar)
+    document.getElementById('closeSidebarBtn').addEventListener('click', closeSidebar)
+}
+
+function openSidebar(){
+    document.getElementById('sidebar').classList.add('active');
+    document.getElementById('pageContent').classList.add('blur');
+}
+
+function closeSidebar(){
+    document.getElementById('sidebar').classList.remove('active');
+    document.getElementById('pageContent').classList.remove('blur');
+}
+
+// 메뉴 동적 생성
 function initNavMenu(){
     const mainMenuItems = [
         { name: "Main Nav 01", dataSection: "subpage"},
@@ -58,17 +68,18 @@ function initNavMenu(){
     createMenuItems(subNavMenu, subMenuItems);
     createMenuItems(sidebar_Main, mainMenuItems);
     createMenuItems(sidebar_Sub, subMenuItems);
+
+    // 생성된 메뉴에 리스너 장착, url 부여
+    const navs = document.querySelectorAll("a[data-section]")
+
+    navs.forEach(nav => {
+        nav.addEventListener("click", (event) => {
+            event.preventDefault(); // 링크의 기본 동작(페이지 이동) 막기
+            const url = nav.getAttribute("data-section"); // URL 가져오기
+            loadPage(url); // 페이지 로드 함수 호출
+        });
+    });
 }
-
-
-
-
-
-
-
-
-
-
 
 // Subpage 로드 함수
 function loadPage(pageName) {
@@ -94,6 +105,7 @@ function loadHTML(url){
         })
         .catch((error) => {
             console.error("페이지 로드 실패:", error);
+            content.innerHTML = "<p>페이지 로드에 실패했습니다. 잠시 후 다시 시도해 주세요.</p>";
         });
 }
 
